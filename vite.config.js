bd5b8b57ddbb0244c50db6535efed84017b26de0
jsh9611 <jangsh9611@naver.com>
2024-06-22 15:05:30 +0900

    vite.config.js에 base path 추가

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/actions-test/' // base 경로를 추가
+/*
+Github는 리소스가 위치한 assets의 기본 루트 경로로 github 저장소 Repository 경로인 `actions-test`를 참조함
+따라서 아래의 경로로 요청할 수 있도록 설정이 필요
+-> https://{github username}.github.io/actions-test/assets/index-해시값.js
+*/
 })
